321f2bb353a21abc741c9d01bba7a1f5da004ee1
Niki Mardari <nikitamardari@gmail.com>
2026-07-12 01:22:57 +0100

    Refactor file handling and variable types in rle.c
    Refactor RLE compression code to improve readability and type safety.

--- Tools/rle.c
+++ Tools/rle.c
@@ -28,29 +28,29 @@ int main(int argc, char *argv[]) {
         fptr_read = fopen(argv[1], "r");
 
         // Check if the file was opened successfully
-        if (fptr_read == NULL) {
+        if (!fptr_read) {
             fprintf(stderr, "Error: Could not open file! %s\n", argv[1]);
             return 1;
         }
         fptr_write = fopen("rle.txt", "w");
 
     // checking if the file is created
-    if (fptr_write == NULL) 
+    if (!fptr_write) 
         fprintf(stderr, "Error: Could not create file rle.txt!\n");
     else 
         fprintf(stdout, "The file is created Successfully.\n");
 
         // RLE logic:
 
-        unsigned int current_val = 0;
+        uint16_t current_val = 0;
         // Read the first hex value from the file
         if (fscanf(fptr_read, "%x,", &current_val) != 1) {
             fclose(fptr_read);
             return 0; // File is empty or improperly formatted
         }
 
-        int count = 1; // Initialize first hex count
-        unsigned int next_val;
+        uint32_t count = 1; // Initialize first hex count
+        uint16_t next_val;
 
     // Loop through the rest of the text file token by token
     while (fscanf(fptr_read, "%x,", &next_val) == 1) {
@@ -61,18 +61,14 @@ int main(int argc, char *argv[]) {
             // Delimiter/Value boundary hit! Print the sequence.
             // printf("%d*0x%04X, ", count, current_val);
 
-    // checking if the file is created
-        fprintf(fptr_write, "%d*0x%04X\n", count, current_val);
+            fprintf(fptr_write, "%d*0x%04X\n", count, current_val);
 
-            
             // Reset values and start agin
             current_val = next_val;
             count = 1;
         }
     }
-
-    // printf("%d*0x%04X\n", count, current_val); // Print the final tracking sequence
-
+        // printf("%d*0x%04X\n", count, current_val); // Print the final tracking sequence
         fclose(fptr_read); // Close the file after reading
         fclose(fptr_write); // Close the output file
 
